c88e94d97687a9479e73e27261dab5b61a14ca19
Zvonimir Rudinski <zvonki12@gmail.com>
2025-10-14 04:16:43 +0200

    feat(supabase): add logging to confirm route

--- app/auth/confirm/route.ts
+++ app/auth/confirm/route.ts
@@ -1,6 +1,7 @@
 import { redirect } from "next/navigation";
 import type { NextRequest } from "next/server";
 import z from "zod";
+import { withLogger } from "@/lib/logger";
 import { createClient } from "@/lib/supabase/server";
 
 const RequestSchema = z.object({
@@ -19,26 +20,28 @@ const RequestSchema = z.object({
 });
 
 export async function GET(request: NextRequest) {
-  const { searchParams } = new URL(request.url);
-  const { token_hash, type, next } = RequestSchema.parse({
-    token_hash: searchParams.get("token_hash"),
-    type: searchParams.get("type"),
-    next: searchParams.get("next"),
-  });
-
-  if (token_hash && type) {
-    const supabase = await createClient();
-
-    const { error } = await supabase.auth.verifyOtp({
-      type,
-      token_hash,
+  return withLogger(request, async (request) => {
+    const { searchParams } = new URL(request.url);
+    const { token_hash, type, next } = RequestSchema.parse({
+      token_hash: searchParams.get("token_hash"),
+      type: searchParams.get("type"),
+      next: searchParams.get("next"),
     });
-    if (!error) {
-      // redirect user to specified redirect URL or root of app
-      redirect(next);
-    }
-  }
 
-  // redirect the user to an error page with some instructions
-  redirect("/error");
+    if (token_hash && type) {
+      const supabase = await createClient();
+
+      const { error } = await supabase.auth.verifyOtp({
+        type,
+        token_hash,
+      });
+      if (!error) {
+        // redirect user to specified redirect URL or root of app
+        redirect(next);
+      }
+    }
+
+    // redirect the user to an error page with some instructions
+    redirect("/error");
+  });
 }
