98b38d2a975262c958f6fd42fb5767f03bfdcf16
Zvonimir Rudinski <zvonki12@gmail.com>
2025-10-17 10:05:47 +0200

    feat(profile): display user info & location

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,11 +1,30 @@
 import { Box } from "@/components/base/box";
+import { Flex } from "@/components/base/flex";
 import { NavigationHeader } from "@/components/navigation/header";
 import { ReceiptScanner } from "@/components/receipt/scanner";
+import { Separator } from "@/components/ui/separator";
+import { getProfileBySupabaseId } from "@/lib/db/profiles/utils";
+import { getSupabaseUser } from "@/lib/supabase/utils";
 
-export default function Profile() {
+export default async function Profile() {
+  const user = await getSupabaseUser();
+  const profile = await getProfileBySupabaseId(user.id);
+
   return (
     <Box>
       <NavigationHeader label="Profile" />
+      <Flex direction="column" className="p-4 gap-2">
+        <p>
+          Name: {profile.firstName} {profile.lastName}
+        </p>
+        <p>Email: {user.email}</p>
+        <p>Gender: {profile.gender}</p>
+        <p>Date of Birth: {profile.dateOfBirth.toDateString()}</p>
+        <Separator />
+        <p>
+          Location: {profile.city}, {profile.state} {profile.zipCode}
+        </p>
+      </Flex>
       <ReceiptScanner />
     </Box>
   );
