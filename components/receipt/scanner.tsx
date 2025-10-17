b8c62b95780a920dc728c53cd0452eed780fcecc
Zvonimir Rudinski <zvonki12@gmail.com>
2025-10-17 10:09:17 +0200

    fix(receipt): remove double bg class

--- components/receipt/scanner.tsx
+++ components/receipt/scanner.tsx
@@ -1,13 +1,12 @@
-import { Avatar } from "@radix-ui/react-avatar";
 import { ScanLine } from "lucide-react";
-import { Box } from "../base/box";
-import { AvatarFallback } from "../ui/avatar";
+import { Box } from "@/components/base/box";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 
 function ReceiptScanner() {
   return (
     <Box className="fixed bottom-8 right-8">
       <Avatar>
-        <AvatarFallback className="size-16 rounded-full bg-white shadow-lg cursor-pointer bg-gray-200 text-gray-600">
+        <AvatarFallback className="size-16 rounded-full shadow-lg cursor-pointer bg-gray-200 text-gray-600">
           <ScanLine />
         </AvatarFallback>
       </Avatar>
