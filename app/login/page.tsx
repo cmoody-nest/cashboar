25d75179340fc11648bf498d512b4637b9a1fed8
Zvonimir Rudinski <zvonki12@gmail.com>
2025-10-17 09:16:29 +0200

    chore(login): add card for login screen

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,29 @@
 import { login, signup } from "@/app/login/actions";
+import { Box } from "@/components/base/box";
 import { LoginForm } from "@/components/login/form";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export default function LoginPage() {
-  return <LoginForm onLogin={login} onSignup={signup} />;
+  return (
+    <Box className="grid min-h-screen place-items-center bg-muted">
+      <Card className="max-w-lg mx-auto w-full">
+        <CardHeader>
+          <CardTitle>CashBoar</CardTitle>
+          <CardDescription>
+            Welcome back! Please log in to your account or sign up to get
+            started.
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="px-6">
+          <LoginForm onLogin={login} onSignup={signup} />
+        </CardContent>
+      </Card>
+    </Box>
+  );
 }
