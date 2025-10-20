e35c4d4a75ea7efb79d1b483894078e54266cf6b
Zvonimir Rudinski <zvonki12@gmail.com>
2025-10-20 14:51:24 +0200

    fix(env): make organization id public

--- lib/env.ts
+++ lib/env.ts
@@ -11,9 +11,7 @@ export const env = createEnv({
     RESEND_WEBHOOK_SECRET: z
       .string()
       .min(1, { message: "RESEND_WEBHOOK_SECRET is required" }),
-    CORESAVE_ORGANIZATION_ID: z
-      .string()
-      .min(1, { message: "CORESAVE_ORGANIZATION_ID is required" }),
+
     CORESAVE_API_KEY: z
       .string()
       .min(1, { message: "CORESAVE_API_KEY is required" }),
@@ -30,6 +28,9 @@ export const env = createEnv({
       .min(1, { message: "NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY is required" }),
     NEXT_PUBLIC_BASE_URL: z.url().default("http://localhost:3000"),
     NEXT_PUBLIC_CORESAVE_API_URL: z.url().default("http://localhost:4000"),
+    NEXT_PUBLIC_CORESAVE_ORGANIZATION_ID: z
+      .string()
+      .min(1, { message: "NEXT_PUBLIC_CORESAVE_ORGANIZATION_ID is required" }),
   },
   runtimeEnv: {
     // Server
@@ -37,7 +38,6 @@ export const env = createEnv({
     NODE_ENV: process.env.NODE_ENV,
     DATABASE_URL: process.env.DATABASE_URL,
     RESEND_WEBHOOK_SECRET: process.env.RESEND_WEBHOOK_SECRET,
-    CORESAVE_ORGANIZATION_ID: process.env.CORESAVE_ORGANIZATION_ID,
     CORESAVE_API_KEY: process.env.CORESAVE_API_KEY,
     // Client
     NEXT_PUBLIC_POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY,
@@ -46,5 +46,7 @@ export const env = createEnv({
       process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY,
     NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
     NEXT_PUBLIC_CORESAVE_API_URL: process.env.NEXT_PUBLIC_CORESAVE_API_URL,
+    NEXT_PUBLIC_CORESAVE_ORGANIZATION_ID:
+      process.env.NEXT_PUBLIC_CORESAVE_ORGANIZATION_ID,
   },
 });
