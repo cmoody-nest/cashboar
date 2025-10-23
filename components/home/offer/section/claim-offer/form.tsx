57a6af46e8424fb4da6319427e8773258750d6b9
Zvonimir Rudinski <zvonki12@gmail.com>
2025-10-23 10:36:17 +0200

    chore(offers): use react hook form to handle offer claiming logic

--- components/home/offer/section/claim-offer/form.tsx
+++ components/home/offer/section/claim-offer/form.tsx
@@ -1,16 +1,52 @@
+"use client";
+
+import { useMutation } from "@tanstack/react-query";
+import { useCallback } from "react";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
 import { claimOffer } from "@/app/actions";
-import { HomeOfferSectionClaimOfferButton } from "@/components/home/offer/section/claim-offer/button";
-import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Form } from "@/components/ui/form";
 import type { Offer } from "@/lib/types/offer";
 
 type Props = Pick<Offer, "id">;
 
 function HomeOfferSectionClaimOfferForm({ id }: Props) {
+  const form = useForm();
+
+  const mutation = useMutation({
+    mutationKey: ["claim-offer", id],
+    mutationFn: async () => {
+      const formData = new FormData();
+      formData.append("offerId", id);
+
+      await claimOffer(formData);
+    },
+    onSuccess: () => {
+      toast.success("Offer claimed successfully!");
+    },
+    onError: (error) => {
+      console.error("Error claiming offer:", error);
+      toast.error("This offer is no longer available.");
+    },
+  });
+
+  const onSubmit = useCallback(() => {
+    mutation.mutate();
+  }, [mutation]);
+
   return (
-    <form action={claimOffer}>
-      <Input type="hidden" name="offerId" value={id} />
-      <HomeOfferSectionClaimOfferButton />
-    </form>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <Button
+          variant="outline"
+          className="cursor-pointer"
+          disabled={mutation.isPending}
+        >
+          Claim Offer
+        </Button>
+      </form>
+    </Form>
   );
 }
 
